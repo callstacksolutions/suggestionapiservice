16da6da930c4d422c93c5e2e241588b9e8254cb1
martelc <martelc@gmail.com>
2017-01-17 19:52:48 -0800

    feature/addDependencyInjection
    - add support for dependency injection via google/guice
    - integrate dependency injection with dropwizard via HubSpot/dropwizard-guice

---
+++ crosscutting/src/main/java/com/callstacksolutions/www/crosscutting/dependencyinjection/CrossCuttingModule.java
@@ -0,0 +1,11 @@
+package com.callstacksolutions.www.crosscutting.dependencyinjection;
+
+import com.google.inject.AbstractModule;
+
+public class CrossCuttingModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
---
+++ crosscutting/src/main/java/com/callstacksolutions/www/crosscutting/dependencyinjection/DataAccessModule.java
@@ -0,0 +1,11 @@
+package com.callstacksolutions.www.crosscutting.dependencyinjection;
+
+import com.google.inject.AbstractModule;
+
+public class DataAccessModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
